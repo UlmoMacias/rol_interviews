3fe77ad6aca6090201e0ff93047d38157b9e84d2
Emiliano Galeana <galeanaara@ciencias.unam.mx>
2019-10-27 08:52:03 -0600

    values of data

---
+++ values.py
@@ -0,0 +1,242 @@
+# data = {'behavior_0000': '0.0', 'behavior_0100': '0.0', 'behavior_0200': '0.0','behavior_0300': '0.0', 'behavior_0400': '0.0', 'behavior_0500': '0.0', 'behavior_0600': '0.0', 'behavior_0700': '0.0', 'behavior_0800': '0.0', 'behavior_0900': '0.0', 'behavior_1000': '0.0', 'behavior_1100': '0.0', 'behavior_1200': '0.0', 'behavior_1300': '0.0', 'behavior_1400': '0.0', 'behavior_1500': '0.0', 'behavior_1600': '0.0', 'behavior_1700': '0.0', 'behavior_1800': '0.0', 'behavior_1900': '0.0', 'behavior_2000': '0.0', 'behavior_2100': '0.0', 'behavior_2200': '0.0', 'behavior_2300': '0.0', 'behavior_friday': '0.0', 'behavior_monday': '0.0', 'behavior_saturday': '0.0', 'behavior_sunday': '0.0', 'behavior_thursday': '0.0', 'behavior_tuesday': '0.0', 'behavior_wednesday': '0.0', 'big5_agreeableness': '0.27833247363234453', 'big5_agreeableness_significant': 'true', 'big5_conscientiousness': '0.22054901910615832', 'big5_conscientiousness_significant': 'true', 'big5_extraversion': '0.35160340822509756', 'big5_extraversion_significant': 'true', 'big5_neuroticism': '3.238456252041555E-5', 'big5_neuroticism_significant': 'true', 'big5_openness': '0.5864367638162304', 'big5_openness_significant': 'true', 'facet_achievement_striving': '0.2856377642617813', 'facet_achievement_striving_significant': 'true', 'facet_activity_level': '0.3589296316610832', 'facet_activity_level_significant': 'true', 'facet_adventurousness': '0.33597521263892244', 'facet_adventurousness_significant': 'true', 'facet_altruism': '0.4793312829533113', 'facet_altruism_significant': 'true', 'facet_anger': '0.6063343701853976', 'facet_anger_significant': 'true', 'facet_anxiety': '0.7400274461981416', 'facet_anxiety_significant': 'true', 'facet_artistic_interests': '0.5649821704903932', 'facet_artistic_interests_significant': 'true', 'facet_assertiveness': '0.36875641811068605', 'facet_assertiveness_significant': 'true', 'facet_cautiousness': '0.39230816693664966', 'facet_cautiousness_significant': 'true', 'facet_cheerfulness': '0.2695654117657871', 'facet_cheerfulness_significant': 'true', 'facet_cooperation': '0.39083087944309436', 'facet_cooperation_significant': 'true', 'facet_depression': '0.8145804184910641', 'facet_depression_significant': 'true', 'facet_dutifulness': '0.42733650821750824', 'facet_dutifulness_significant': 'true', 'facet_emotionality': '0.585784195114429', 'facet_emotionality_significant': 'true', 'facet_excitement_seeking': '0.5208889689748948', 'facet_excitement_seeking_significant': 'true', 'facet_friendliness': '0.31184264830244013', 'facet_friendliness_significant': 'true', 'facet_gregariousness': '0.40286403099663376', 'facet_gregariousness_significant': 'true', 'facet_imagination': '0.6401557248879028', 'facet_imagination_significant': 'true', 'facet_immoderation': '0.788897371083707', 'facet_immoderation_significant': 'true', 'facet_intellect': '0.7619010029092169', 'facet_intellect_significant': 'true', 'facet_liberalism': '0.8511814913060672', 'facet_liberalism_significant': 'true', 'facet_modesty': '0.566174765853829', 'facet_modesty_significant': 'true', 'facet_morality': '0.34558512332585084', 'facet_morality_significant': 'true', 'facet_orderliness': '0.22785973733498294', 'facet_orderliness_significant': 'true', 'facet_self_consciousness': '0.719377181977424', 'facet_self_consciousness_significant': 'true', 'facet_self_discipline': '0.19624329027637866', 'facet_self_discipline_significant': 'true', 'facet_self_efficacy': '0.31133045879474097', 'facet_self_efficacy_significant': 'true', 'facet_sympathy': '0.6143513378009484', 'facet_sympathy_significant': 'true', 'facet_trust': '0.3991115231187055', 'facet_trust_significant': 'true', 'facet_vulnerability': '0.7263644219593869', 'facet_vulnerability_significant': 'true', 'need_challenge': '0.3034657182346173', 'need_challenge_significant': 'true', 'need_closeness': '0.33223591591984014', 'need_closeness_significant': 'true', 'need_curiosity': '0.3770866603899191', 'need_curiosity_significant': 'true', 'need_excitement': '0.3847777666967629', 'need_excitement_significant': 'true', 'need_harmony': '0.2643216589278793', 'need_harmony_significant': 'true', 'need_ideal': '0.2966039968185179', 'need_ideal_significant': 'true', 'need_liberty': '0.32333903525987506', 'need_liberty_significant': 'true', 'need_love': '0.4140512833177682', 'need_love_significant': 'true', 'need_practicality': '0.24459039211369554', 'need_practicality_significant': 'true', 'need_self_expression': '0.30789762559402006', 'need_self_expression_significant': 'true', 'need_stability': '0.21067254480097503', 'need_stability_significant': 'true', 'need_structure': '0.25553312507271236', 'need_structure_significant': 'true', 'processed_language': 'en', 'value_conservation': '0.16940062820796137', 'value_conservation_significant': 'true', 'value_hedonism': '0.4213398431648047', 'value_hedonism_significant': 'true', 'value_openness_to_change': '0.46539732214273516', 'value_openness_to_change_significant': 'true', 'value_self_enhancement': '0.37768584654241755', 'value_self_enhancement_significant': 'true', 'value_self_transcendence': '0.3833313006690231', 'value_self_transcendence_significant': 'true', 'word_count': '102',}
+
+communicative_list = [ 'big5_agreeableness',
+                       'big5_conscientiousness',
+                       'big5_extraversion',
+                       'big5_openness',
+                       'facet_altruism',
+                       'facet_assertiveness',
+                       'facet_emotionality',
+                       'facet_friendliness',
+                       'facet_gregariousness',
+                       'facet_intellect',
+                       'facet_modesty',
+                       'facet_sympathy',
+                       'facet_trust',
+                       'need_practicality',]
+
+communicative_neg_list = ['facet_anger',
+                          'facet_anxiety',
+                          'facet_depression',
+                          'facet_excitement_seeking',
+                          'facet_vulnerability',]
+
+collaborative_list = [ 'big5_extraversion',
+                       'big5_openness',
+                       'facet_achievement_striving',
+                       'facet_activity_level',
+                       'facet_adventurousness',
+                       'facet_altruism',
+                       'facet_assertiveness',
+                       'facet_cheerfulness',
+                       'facet_dutifulness',
+                       'facet_excitement_seeking',
+                       'facet_gregariousness',
+                       'facet_imagination',
+                       'facet_modesty',
+                       'facet_morality',
+                       'facet_orderliness',
+                       'facet_self_discipline',
+                       'facet_self_efficacy',
+                       'facet_sympathy',
+                       'need_challenge',
+                       'need_excitement',
+                       'need_harmony',
+                       'need_liberty',
+                       'value_self_enhancement',
+                       'value_self_transcendence',]
+
+collaborative_neg_list = [ 'big5_neuroticism',
+                           'facet_anger',
+                           'facet_anxiety',
+                           'facet_depression',
+                           'facet_immoderation',
+                           'need_closeness',]
+
+collaborative_pos_list = [ 'facet_cooperation',
+                           'facet_friendliness',
+                           'facet_trust',
+                           'value_openness_to_change',]
+
+accountability = [ 'big5_conscientiousness',
+                   'facet_achievement_striving',
+                   'facet_activity_level',
+                   'facet_cautiousness',
+                   'facet_cheerfulness',
+                   'facet_emotionality',
+                   'facet_imagination',
+                   'facet_morality',
+                   'facet_self_consciousness',
+                   'facet_self_discipline',
+                   'facet_trust',
+                   'need_curiosity',
+                   'need_liberty',
+                   'need_practicality',
+                   'need_self_expression',
+                   'value_openness_to_change',
+                   'value_self_enhancement',
+                   'value_self_transcendence',]
+
+accountability_neg = [ 'big5_neuroticism',
+                   'facet_anxiety',
+                   'facet_immoderation',
+                   'need_closeness',
+                   'value_hedonism',]
+
+drive = [ 'big5_extraversion',
+          'big5_openness',
+          'facet_adventurousness',
+          'facet_altruism',
+          'facet_assertiveness',
+          'facet_cautiousness',
+          'facet_cooperation',
+          'facet_emotionality',
+          'facet_excitement_seeking',
+          'facet_gregariousness',
+          'facet_intellect',
+          'facet_modesty',
+          'facet_morality',
+          'facet_orderliness',
+          'facet_self_consciousness',
+          'facet_self_discipline',
+          'facet_self_efficacy',
+          'need_curiosity',
+          'need_harmony',
+          'need_liberty',
+          'need_practicality',
+          'need_self_expression',
+          'need_stability',
+          'value_openness_to_change',]
+
+drive_neg = [ 'big5_neuroticism',
+              'facet_anger',
+              'facet_depression',
+              'facet_immoderation',
+              'facet_vulnerability',
+              'need_closeness',
+              'value_hedonism',]
+
+
+# print(data)
+
+
+def values(data : dict):
+    comm = 0
+    coll = 0
+    d = 0
+    a = 0
+    for var in communicative_list:
+        val = float(data.get(var))
+        comm = comm + val
+
+    for var in communicative_neg_list:
+        val = float(data.get(var))
+        comm = comm - val        
+
+    for var in collaborative_list:
+        val = float(data.get(var))
+        coll = coll + val
+
+    for var in collaborative_neg_list:
+        val = float(data.get(var))
+        coll = coll - val
+        
+    for var in collaborative_pos_list:
+        val = float(data.get(var))
+        coll = coll + val*2
+
+    for var in accountability:
+        val = float(data.get(var))
+        a = a + val
+
+    for var in accountability_neg:
+        val = float(data.get(var))
+        a = a - val
+
+    for var in drive:
+        val = float(data.get(var))
+        d = a + val
+
+    for var in drive_neg:
+        val = float(data.get(var))
+        d = a - val
+
+    print(comm)
+    print(coll)
+    print(a)
+    print(d)
+
+    comm = comm/(len(communicative_list)+len(communicative_neg_list))
+    coll = coll/(len(collaborative_list)+len(collaborative_neg_list)+len(collaborative_pos_list))
+    a = a/(len(accountability)+len(accountability_neg))
+    d = d/(len(drive)+len(drive_neg))
+
+    print(">>>>>>")
+
+    print(comm)
+    print(coll)
+    print(a)
+    print(d)
+
+    print(">>>>>>")
+
+
+    list_pre = {'Accountability':'0', 'Drive': '0', 'Collaborative Spirit': '0', 'Communicative for Impact': '0'}
+    
+    if comm > 0.0 and comm < .16666:
+        list_pre.update({'Communicative for Impact': "Not Assessed"})
+    elif comm > .16666 and comm < .333333:
+        list_pre.update({'Communicative for Impact': "Unacceptable"})
+    elif comm > .333333 and comm < .50000:
+        list_pre.update({'Communicative for Impact': "Margina"})
+    elif comm > .50000 and comm < .66666666:
+        list_pre.update({'Communicative for Impact': "Acceptable"})
+    elif comm > .66666666 and comm < .83333333:
+        list_pre.update({'Communicative for Impact': "Above Average"})
+    else:
+        list_pre.update({'Communicative for Impact': "Outstanding"})
+
+    if coll > 0.0 and coll < .16666:
+        list_pre.update({'Collaborative Spirit': "Not Assessed"})
+    elif coll > .16666 and coll < .333333:
+        list_pre.update({'Collaborative Spirit': "Unacceptable"})
+    elif coll > .333333 and coll < .50000:
+        list_pre.update({'Collaborative Spirit': "Margina"})
+    elif coll > .50000 and coll < .66666666:
+        list_pre.update({'Collaborative Spirit': "Acceptable"})
+    elif coll > .66666666 and coll < .83333333:
+        list_pre.update({'Collaborative Spirit': "Above Average"})
+    else:
+        list_pre.update({'Collaborative Spirit': "Outstanding"})
+
+    if a > 0.0 and a < .16666:
+        list_pre.update({'Accountability': "Not Assessed"})
+    elif a > .16666 and a < .333333:
+        list_pre.update({'Accountability': "Unacceptable"})
+    elif a > .333333 and a < .50000:
+        list_pre.update({'Accountability': "Margina"})
+    elif a > .50000 and a < .66666666:
+        list_pre.update({'Accountability': "Acceptable"})
+    elif a > .66666666 and a < .83333333:
+        list_pre.update({'Accountability': "Above Average"})
+    else:
+        list_pre.update({'Accountability': "Outstanding"})
+
+    if d > 0.0 and d < .16666:
+        list_pre.update({'Drive': "Not Assessed"})
+    elif d > .16666 and d < .333333:
+        list_pre.update({'Drive': "Unacceptable"})
+    elif d > .333333 and d < .50000:
+        list_pre.update({'Drive': "Margina"})
+    elif d > .50000 and d < .66666666:
+        list_pre.update({'Drive': "Acceptable"})
+    elif d > .66666666 and d < .83333333:
+        list_pre.update({'Drive': "Above Average"})
+    else:
+        list_pre.update({'Drive': "Outstanding"})        
+        
+    print(list_pre)
+    return list_pre
+
+
+values()
